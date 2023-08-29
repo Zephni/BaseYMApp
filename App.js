8ba18d603f1bf5e86ce47fcc6c6f49fd64157cd9
Craig Dennis <zephni@hotmail.co.uk>
2023-08-29 13:53:37 +0100

    Now interacts between web app and react natiev. Auto login etc

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [url, setUrl] = useState('https://ymwa.deliverysoftware.co.uk/set-pdms-db/demo_pdms852');
+
+  const injectedJavaScript = `
+  (function() {
+    // If ReactNativeWebView exists
+    if (window.ReactNativeWebView) {
+      // Listen for the successfulRegistration event
+      window.addEventListener('successfulRegistration', function(event) {
+        // Post the message to React Native
+        window.ReactNativeWebView.postMessage(JSON.stringify(event.detail));
+      });
+    }else{
+      alert('ReactNativeWebView not found, unable to speak to React Native');
+    }
+  })();
+`;
+  const handleMessage = async (event) => {
+    let customerId = JSON.parse(event.nativeEvent.data);
+    await AsyncStorage.setItem('customerId', customerId);
+  };
+
+  // If app is loaded and already has customerId, then change the source of the webview
+  AsyncStorage.getItem('customerId').then((customerId) => {
+    if (customerId !== null) {
+      setUrl('https://ymwa.deliverysoftware.co.uk/set-pdms-db-and-linked-account/demo_pdms852/' + customerId);
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ width: '100%', height: '100%', backgroundColor: '#EEEEEE' }}>
+      <StatusBar style="light" backgroundColor="#1F80E4" translucent={false} />
+      <WebView
+        originWhitelist={['http://*', 'https://*', 'about:srcdoc']}
+        source={{ uri: url }}
+        scrollEnabled={true}
+        setBuiltInZoomControls={false}
+        setDisplayZoomControls={false}
+        style={{ width: '100%', resizeMode: 'contain' }}
+        injectedJavaScriptBeforeContentLoaded={injectedJavaScript}
+        onMessage={handleMessage}
+        />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
